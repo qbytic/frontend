c2a782e094be5388f3ea7f8dffffd0c037f78876
me <bhavesh.pareek36@gmail.com>
2020-07-28 12:34:44 +0530

    update color

--- src/components/shared/Logo.tsx
+++ src/components/shared/Logo.tsx
@@ -10,12 +10,7 @@ export function Logo(props: LogoProps) {
 
   return (
     <A href="/" class={["hoverable", "qbytic-logo"].concat(props.class)}>
-      <svg
-        xmlns="http://www.w3.org/2000/svg"
-        viewBox="0 0 257.25 257.25"
-        height={height}
-        width={width}
-      >
+      <svg viewBox="0 0 257.25 257.25" height="100" width="100">
         <defs>
           <linearGradient
             id="A"
@@ -25,8 +20,8 @@ export function Logo(props: LogoProps) {
             y2="550.23"
             gradientUnits="userSpaceOnUse"
           >
-            <stop offset="0" stop-color="#56f8ff" />
-            <stop offset=".84" stop-color="#008aa2" />
+            <stop offset="0" stop-color="var(--qbytic-blue)"></stop>
+            <stop offset=".84" stop-color="#008aa2"></stop>
           </linearGradient>
           <linearGradient
             id="B"
@@ -36,8 +31,8 @@ export function Logo(props: LogoProps) {
             y2="433.58"
             gradientUnits="userSpaceOnUse"
           >
-            <stop offset=".3" stop-color="#56f8ff" />
-            <stop offset="1" stop-color="#5f4dff" />
+            <stop offset=".3" stop-color="var(--qbytic-blue)"></stop>
+            <stop offset="1" stop-color="#5f4dff"></stop>
           </linearGradient>
           <radialGradient
             id="C"
@@ -46,28 +41,28 @@ export function Logo(props: LogoProps) {
             r="128.62"
             gradientUnits="userSpaceOnUse"
           >
-            <stop offset=".82" stop-color="#00d4d1" />
-            <stop offset="1" stop-color="#007594" />
+            <stop offset=".82" stop-color="var(--qbytic-blue)"></stop>
+            <stop offset="1" stop-color="#007594"></stop>
           </radialGradient>
         </defs>
         <g transform="translate(-169.35 -291.71)">
           <path
             fill="url(#A)"
             d="M426.52 420.34c0 58.05-70.5 128.54-128.54 128.54h128.54V420.34z"
-          />
+          ></path>
           <path
             fill="none"
             stroke="url(#C)"
             stroke-miterlimit="10"
             stroke-width="47"
             d="M298 525.46a105.13 105.13 0 11105.1-105.13"
-          />
+          ></path>
           <path
             fill="url(#B)"
             d="M298.35 420.33L298 502s80.1 2.5 81.58-81.27z"
-          />
+          ></path>
         </g>
-      </svg>
+      </svg>{" "}
     </A>
   );
 }
